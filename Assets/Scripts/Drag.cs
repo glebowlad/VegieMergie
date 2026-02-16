d5d33eae0011f499f0ecfea12d77078113b61ba1
Glebov Vlad <glebowladic@gmail.com>
2026-02-16 20:28:05 +0300

    удалил лишнее

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -26,35 +26,19 @@ public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
         spawnerWidth= rectTransform.rect.width;
         line = transform.GetChild(0);
         line.gameObject.SetActive(false);
-       // CalculateLimits();
 
 
     }
-
-    //private void CalculateLimits()
-    //{
-    //    Vector2 leftWallScreenPos = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera,leftWall.position);
-    //    Vector2 rightWallScreenPos = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera,rightWall.position);
-    //    Vector2 selfScreenPos = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera,transform.position);
-
-    //   spawnerWidth = (rectTransform.rect.width * canvas.scaleFactor)/2f;
-    //    minX = leftWallScreenPos.x + spawnerWidth;
-    //    maxX = rightWallScreenPos.x - spawnerWidth;
-    //    Debug.Log($"minX {minX}");
-    //    Debug.Log($"maxX {maxX}");
-       
-
-    //}
 public void OnBeginDrag(PointerEventData eventData)
     {
         //DragBegined?.Invoke();
-        line.gameObject.SetActive(true);
-       // CalculateLimits();
+      
     }
     public void OnDrag(PointerEventData eventData)
     {
 
         WhileDrag?.Invoke();
+        line.gameObject.SetActive(true);
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
         canvas.transform as RectTransform,
         eventData.position,
@@ -63,9 +47,7 @@ public void OnBeginDrag(PointerEventData eventData)
         // Преобразуем позиции стен в локальные координаты канваса
         Vector2 leftWallLocal = canvas.transform.InverseTransformPoint(leftWall.position);
         Vector2 rightWallLocal = canvas.transform.InverseTransformPoint(rightWall.position);
-        //Debug.Log($"Left {leftWallLocal}");
-        //Debug.Log($"Right {rightWallLocal}");
-        // Учитываем половину ширины объекта
+        // Учитываем долю ширины объекта
         float halfWidth = rectTransform.rect.width/1.5f;
 
         // Ограничиваем движение
